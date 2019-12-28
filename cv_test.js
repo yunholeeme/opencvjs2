465033f07e32d19f32b21c32234a69816c3b20ac
hodu <neuezeal@gmail.com>
2019-12-28 16:54:58 +0900

    feat: add converting srcimage to grayscale image.
    converting srcimage to grayscale image

--- cv_test.js
+++ cv_test.js
@@ -4,6 +4,21 @@ inputElement.addEventListener("change", (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
+imgElement.onload = function() {
+    let mat = cv.imread(imgElement);
+    let dst = new cv.Mat();
+
+    cv.cvtColor(mat, dst, cv.COLOR_RGBA2GRAY);
+    cv.imshow('canvasOutput', dst);
+    mat.delete();
+};
+
+const p = document.querySelector(".js-status");
+
+function onOpenCvReady() {
+    p.innerHTML = "OpenCV.js is ready.";
+}
+
 function init() {
 
 }
